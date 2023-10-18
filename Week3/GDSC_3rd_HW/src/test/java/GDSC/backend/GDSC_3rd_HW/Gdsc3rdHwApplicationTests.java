0dc93d63cd7e381bf4136cfc0b0668812d4c4edc
yunhacandy <yunhacandy@g.hongik.ac.kr>
2023-10-18 22:48:21 +0900

    .

--- Week3/GDSC_3rd_HW/src/test/java/GDSC/backend/GDSC_3rd_HW/Gdsc3rdHwApplicationTests.java
+++ Week3/GDSC_3rd_HW/src/test/java/GDSC/backend/GDSC_3rd_HW/Gdsc3rdHwApplicationTests.java
@@ -1,8 +1,10 @@
+package GDSC.backend.GDSC_3rd_HW;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class Gdsc3rdHwApplicationTests {
 
 	@Test
 	void contextLoads() {
